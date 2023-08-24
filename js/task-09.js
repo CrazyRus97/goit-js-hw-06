cd21fd3185e6fa03e9c4466553b9612ad38d3c5e
Fedorenko Ruslan <130165394+CrazyRus97@users.noreply.github.com>
2023-08-24 17:15:42 +0300

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const buttonMain = document.querySelector('.change-color')
+const bodyMain = document.querySelector('body')
+const spanMain = document.querySelector('.color')
+
+const btnHandler = (event) => {
+  spanMain.textContent = getRandomHexColor()
+  bodyMain.style.backgroundColor = getRandomHexColor()
+}
+
+buttonMain.addEventListener("click", btnHandler)
