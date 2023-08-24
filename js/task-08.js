3564651b3512084ea3792eee448ba65dd927f5a3
Fedorenko Ruslan <130165394+CrazyRus97@users.noreply.github.com>
2023-08-24 17:09:51 +0300

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formMain = document.querySelector('.login-form')
 
+const submitHandler = (event) => {
+    event.preventDefault();
+    if (event.currentTarget.elements.email.value === '' || event.currentTarget.elements.password.value === '') {
+        window.alert('All forms inputs must be added!')
+    }
+    else {
+        const { email, password } = event.currentTarget.elements;
+        const info = {
+            email: email.value,
+            password: password.value,
+        }
+        console.log(info)
+    }
+
+};
+
+formMain.addEventListener("submit", submitHandler)
