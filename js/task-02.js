b58e801a9b77fd5ee32f9eede1135bb7f1df6ac0
Fedorenko Ruslan <130165394+CrazyRus97@users.noreply.github.com>
2023-09-23 14:09:56 +0300

    task-02 check

--- js/task-02.js
+++ js/task-02.js
@@ -10,12 +10,14 @@ const ingredients = [
 const mainUlList = document.querySelector('#ingredients')
 
 function secondFunction(ingredients) {
-  ingredients.forEach(ingredient => {
-    const liList = document.createElement('li')
-    liList.textContent = ingredient
-    liList.className = 'item'
-    mainUlList.append(liList)
-  });
+  const ingredientsArr = ingredients.map(ingredient => `<li style="display:'flex'; flex-direction:column; class="item">${ingredient}</li>`).join("");
+  // ingredients.forEach(ingredient => {
+  //   const liList = document.createElement('li')
+  //   liList.textContent = ingredient
+  //   liList.className = 'item'
+  //   mainUlList.append(liList)
+    mainUlList.insertAdjacentHTML("afterbegin", ingredientsArr)
+  // });
 }
 
 secondFunction(ingredients)
